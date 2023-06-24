f29ef012389a99ec8a9ef321510495c95787af0d
appfine6 <108558202+appfine6@users.noreply.github.com>
2023-06-24 23:53:11 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
-<script type="text/javascript">
-window.location="https://inspiring-toffee-0e7f80.netlify.app/";
+<script>
+function delayRedirect(){
+    document.getElementById('delayMsg').innerHTML = 'Please wait you'll be redirected after <span id="countDown">5</span> seconds....';
+    var count = 5;
+    setInterval(function(){
+        count--;
+        document.getElementById('countDown').innerHTML = count;
+        if (count == 0) {
+            window.location = 'https://www.google.com'; 
+        }
+    },1000);
+}
 </script>
